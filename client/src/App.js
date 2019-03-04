79930b42c1126aa1e7586ec1927b6f34175b1a74
shailendra <shail5788@gmail.com>
2019-03-05 02:05:10 +0530

    add login and registration

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./component/layout/Navbar";
+import Footer from "./component/layout/Footer";
+import Landing from "./component/layout/Landing";
+import Login from "./component/auth/Login";
+import Register from "./component/auth/Register";
 
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>This is my first app</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
